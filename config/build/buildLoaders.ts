b4d977a1d4f8ecad53e054088a8fcd93dc0fa362
vovka <lesnikov.vladimir@gmail.com>
2024-01-26 15:00:51 +0300

    add babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,6 +7,23 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        { locales: ['ru', 'en'], keyAsDefaultValue: true },
+                    ],
+                ],
+            },
+        },
+    };
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -42,5 +59,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
--- src/Pages/AboutPage/ui/AboutPage.tsx
+++ src/Pages/AboutPage/ui/AboutPage.tsx
@@ -3,7 +3,11 @@ import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
-    return <div>{t('О сайте')}</div>;
+    return (
+        <div>
+            <div>{t('О сайте')}</div>
+        </div>
+    );
 };
 
 export default AboutPage;
